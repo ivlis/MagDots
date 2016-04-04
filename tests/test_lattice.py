f37718a9e0c26797fe0a79409ce8e9ee5fdb06a3
Ivan Lisenkov <lisenkov@oakland.edu>
2016-04-04 11:58:23 -0400

    New lattice tests

--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -7,6 +7,13 @@ import unittest
 from ..model.lattice import Lattice
 
 class TestLattice(unittest.TestCase):
+    def test_properties(self):
+        a1 = np.array([2.0, 0.0, 0.0])
+        a2 = np.array([0.0, 0.5, 0.0])
+        la = Lattice(a1, a2)
+        npt.assert_allclose(a1, la.a1, atol=1e-7)
+        npt.assert_allclose(a2, la.a2, atol=1e-7)
+
     def test_rectangular(self):
         a1 = np.array([2.0, 0.0, 0.0])
         a2 = np.array([0.0, 0.5, 0.0])
@@ -37,3 +44,13 @@ class TestLattice(unittest.TestCase):
         b2 = la.b2
         npt.assert_allclose([a1.dot(b2), a2.dot(b1)], np.zeros(2), atol=1e-7)
         npt.assert_allclose([a1.dot(b1), a1.dot(b1)], [2*np.pi, 2*np.pi], atol=1e-7)
+
+    def test_GXM(self):
+        a1 = np.array([1.0, 0.0, 0.0])
+        a2 = np.array([0.0, 1.0, 0.0])
+        la = Lattice(a1, a2)
+        (G, X, M) = la.GXM
+        X_should_be = la.b1/2
+        M_should_be = (la.b1 + la.b2)/2
+        npt.assert_allclose(X,X_should_be)
+        npt.assert_allclose(M,M_should_be)
